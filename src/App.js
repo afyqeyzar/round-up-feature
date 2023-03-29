09ff3c77d10cac8b7934da942827dbf26db742a6
afyqeyzar <norafyqe@gmail.com>
2023-03-29 14:21:21 +0100

    fetching API method now works

--- src/App.js
+++ src/App.js
@@ -4,29 +4,22 @@ import { useEffect } from "react"
 
 const App = () => {
 
-  const getAPI = async () => {
-    console.log(process.env);
+  const getAccountsAPI = async () => {
+    // console.log(process.env);
     const response = await fetch('/api/v2/accounts', {
       method: "GET",
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_STARLING_ACCESS_TOKEN}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        // 'User-Agent': 'Nor Afyq Eyzar'
       },
-      mode: 'no-cors',
-      withCredentials: true,
-      credentials: 'include'
-      });
-    const data = await response.json();
+    });
+    const accountsData = await response.json();
 
-    console.log(data)
-
-    
+    console.log(accountsData.accounts[0])
   };
 
+
   useEffect(() => {
-    getAPI();
+    getAccountsAPI();
   }, []);
 
   return (
