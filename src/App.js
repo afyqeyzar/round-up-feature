241891c627251ef3228326b9d73e315b70cfd046
afyqeyzar <norafyqe@gmail.com>
2023-03-29 10:35:26 +0100

    trying to figure out API request

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import { useEffect } from "react"
 // import { get } from 'https';
 // import { react } from '@babel/types';
 
-const ACCESS_TOKEN = 'eyJhbGciOiJQUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAA_31Uy5KbMBD8lS3Oqy0wjwVuueUH8gHj0chWWUiUJLxxpfLvEUgY43Xl2N3z6NEM_Mmkc1mfwSgZp8F8OA9WSX06gr58oBmy98xNxxDR1WXFi7xj8CkqVhWYM6BDw7Bq6qI-5G1OGILp95j1RdPmRVl8Nt17JsFHIq_qciYA0Uza_zSKk_0leah96ASnAwLrulCxEkXO2jovmaiaoqtzzHlTh9reXEjHjKoQFRR5CP4sS1ZVgKxtsWO8rnjLy0o01IaMMNYPRHIuZnWlaPjx0DKEIqTmZc26ogZGbdkWbXEQods8MJqR5keJThkq44j3loC_rdx5sc80DPRS8LfxSZCctJdCkt3zSjq_YxLg3AbjPXHp7yAq3gOeB7pHbvjLSk9vMPmzsdKFNTKpubxKPoGKwUdQoDFZQ7CcodHeGhUbzUzSjBbSDuCl0cwIJibN3V1y9-4riK1xct4M64g0gEyFFQUj-tTDOKrbHS1RA2gOnnpOikKJFSbNXsjPg4yWBFkK3t3_pGgjaqMCpPACnk52meMx8buYUsniGdbpBvIQ3ECPAS5qwstQI9yIVimCNEQEWxCTA5zSTIkIna1e1xLjH2RvQTvAzXWg2XFSl37dLm3U5iDizUTEa4H5RsK9DdJvNZXBYOKhwkIwMx_JM5uyrBFSrSPFGXfUEmUJSY5-B9xeig--LM-CSqKDa9ipYyez2dpxabgd9y0zvl8o_KrEJr6otYmxKJ6JT4o4S--XaPI-zDuNCY6wflPhB7rcNjOWP7Tfs2vfPfsin5kvfec9LQtEd32mRi4SNR0d2vC4892sXR65JerxuJb9PV9b9vcf7ca26RkGAAA.Zwnd0QF-RZxDW_J9X4kI6lqf_DZK2q1TCW_ieGuVUdW4XdJ1PWUz1w_e3wLn02o5EPwUQUvQdxCocBsYUzRRJ0UK2yKeo97bZfn9XECebgIEFDRn0K81bIut7U8E8P4Cws9Y3-HkYdvAFMtA1VQFrIEq8N54UMmMyYfy0cDfcPEeOZuNTH-tuLz6l2H6OaaWV94a9xEEbGN7J07NcjdaXGeOexiXeqkQH2uNdWVKmZcNQcV3ZNIy5Be7q84K5WXS_b3lRdjmhwp9klBtShJW2yosfMmz7oGtsfLZY0YzOlTUavl5ct93zeNKehpjEJaLlXSYxOR2UbyRqRh8JPskCIAvmVZog1Lls-FCNSKgZHyL4mleZjsngYSPm9QQnz5941kNvKoOiadPGM97rExz0yKY9LBNulC_Jc4FKHcOGQNMZRyiHQ4d-_dZSMm5CGBG-siImkMtadjW9IMyMfFycWiQ_DDvzbttCT63O3xrC0EV7R58BXEenGAOy3RBQNCqva32JSps_hGn-d_ZHv3dx68y18jDOn38BxWHGUNHvxYviHHTnfboRZ3JiPNZFOalpOCdSB1KR8x-0IbxUDXnAjJ5094kJyoBwd-dxEKnyVsjnamzAo9cWYSittkB4UhoYRUynGdG5IBv-jPazwrVd-OnmcsENUbWPM7bbKvsKGk';
-const bearer = 'Bearer' + ACCESS_TOKEN;
+const ACCESS_TOKEN = 'eyJhbGciOiJQUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAA_31Uy5KbMBD8lS3Oqy3xXOCWW34gHzBII1tlIVGS8MaVyr9HIGGM15Vjd8-jRzPwJ5POZX0GkyQcR_PhPFgl9WkAfflgZszeMzcPIaKry4rntCPwKSpS5YwSwKIhrGrqvC5oS5GFYPw9ZX3etDQv6yJv3jMJPhK0atuFAMbMrP1PozjaX5KH2kUnOBYMSNeFipXIKWlrWhJRNXlXU0Z5U4fa3lxQx4yqKHhVi5YUnwMnFW0G0nZFRZDTjtG6_BxoGTLCWD8YQ-diVleKhg9FSxjkoQ8ta9LlNRBsyzZv80KEbsvAzEy4PEp0SpgyDnlvEfjbxp1X-0TDiC8Ff5ueBMlReykk2iOvpPMHJgHObTDeI5f-DqLiPbDziPfIHX9Z6fENZn82VrqwRiI1l1fJZ1AxeAAFmiVrDCwnzGhvjYqNFiZpRgtpR_DSaGIEEbPm7i65e_cNxNZsdt6M24g4gkyFFQYj-tTDNKnbHa1RI2gOHnuOCkOJDSbNXtAvg0wWBVoM3t3_pGgjapMChuEFPJ7sOsdj4ncxpaJlZ9imG9FDcAM9C3BVE16HmuCGuEkRpCEi2IOIHOGUZkpE6Gz1tpYY_yB7C9oB210HmgyzuvTbdnGndgcR7yYi3gosNxLubZR-r6kMCyYeKqwEMcuRPLMpyxoh1TZSnPFArVEWGcrJH4A7SvHB1-VZUEl0cA07deRkdlsHLg134L5lxvcLhV-V2MUXtXYxFmVn5LNCTtL7JRq9D_POU4ITbN9U-IGut02M5Q_tj-zW98i-yCfmS995j-sCmbs-UxMXiZoHx2x43OVuti6P3Br1eFzr_p6vLfv7D75ixkQZBgAA.vObOD775F3-2S3teAAx0QVa1htiaco0UBO43sbQ4ZyBdebAI5MlXwVqIDdCa54u8Iw8MCscHeroJzzdWqneKJ59cmwD-M-u8JjrrD9avw-YzWsm3uUW5b1uC_IM-La6goLoUvCafdKnTYNHNLxxH080939153OSaWwcBlItMzqxXcO4zDKn1o2SbBLkH6F7H3tYOnuKrhQBXKQpu6FYGcwwx-TCuh8Q0uFtOUWyYxb2mELJOgtfWXqhn9sdqeaftnOpN3942RY0f80B4gN8FP0HuHtmBXjISBf6wJMOqPmxvHHkZvH-IS3AajmOrBo8p9SSBJ6JAAibwKUDp3d-vHjChxRehJU4gLLR4TnYkRyn313mAJC9bOxV-Fm773Lrvj5vPR8F1IZGwv5ydhsvXBS_rOWK1wuYPGK9hcvMfoHwMdm59rCJqwA81NY4zXYSh5stfwV8bYko7d21f6jdtS9tL_oC5PhTZnzx43OUtnmmf-MXsLcpGLcTBYstpBqkoX99tzhHQY5B5nzLkxdrnS8vqv6v0gUuyMujvRd44fRbBKPonbV09mIPW0Xosr6l14g0KM1qIjWWU2K3M-JjsiRYUt03Q3drCIaD3VXyoo1og387iu6K_I4ycK5PGdG6n7hGinY516Q1NVsDD9U4FyO8lpJeTqMDG9DRkzaF_sxg';
+const bearer = 'Bearer ' + ACCESS_TOKEN;
 
-function App() {
+const App = () => {
 
   const getAPI = async () => {
+    // console.log(bearer);
     const response = await fetch('https://api-sandbox.starlingbank.com/api/v2/accounts', {
       method: "GET",
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        // 'User-Agent': 'Nor Afyq Eyzar'
+      },
       mode: 'no-cors',
       withCredentials: true,
-      credentials: 'include',
-      headers: {
-        'Accept': 'application/json',
-        "Content-Type": "application/json",
-        'Authorization': bearer
-      }
+      credentials: 'include'
       });
     const data = await response.json();
 
-    console.log('test');
+    console.log(data)
+
+    
   };
 
   useEffect(() => {
