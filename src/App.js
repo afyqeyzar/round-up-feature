ffd7d3b5b8a5cee476b8ee6ed49b1419d8a8be22
afyqeyzar <norafyqe@gmail.com>
2023-03-29 16:00:08 +0100

    fetching Balance and identifiers API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { assertTSExternalModuleReference } from "@babel/types";
 import { useEffect, useState } from "react"
 import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
 import getAccountsAPI from "./APIMethods"
 
 const App = () => {
 
-  const [searchTerm, setSearchTerm] = useState('first')
+  // const STATIC_ACCOUNT_UID = 'b47101a3-dce8-48ea-89c5-ccb3ad486caf'
+
+  const [accountUid, setAccountUid] = useState([])
 
   const getAccountsAPI = async () => {
     const response = await fetch('/api/v2/accounts', {
@@ -14,20 +17,48 @@ const App = () => {
       },
     });
     const accountsData = await response.json();
+    
 
     const accountUid = accountsData.accounts[0].accountUid;
-    return accountUid 
+    setAccountUid(accountUid);
+    return accountUid; 
+  };
+
+  const getAccountsIdentifiersAPI = async () => {
+    const accountUid = await getAccountsAPI();
+    const response = await fetch(`/api/v2/accounts/${accountUid}/identifiers`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_STARLING_ACCESS_TOKEN}`,
+      },
+    });
+    const accountsIdentifiersData = await response.json();
+
+    console.log(accountsIdentifiersData)
+  };
+
+  const getAccountsBalanceAPI = async () => {
+    const accountUid = await getAccountsAPI();
+    const response = await fetch(`/api/v2/accounts/${accountUid}/balance`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_STARLING_ACCESS_TOKEN}`,
+      },
+    });
+    const accountsBalanceData = await response.json();
+
+    console.log(accountsBalanceData)
   };
 
 
   useEffect(() => {
-    const b = getAccountsAPI();
-    console.log(b);
+    // getAccountsAPI();
+    getAccountsBalanceAPI();
   }, []);
 
   return (
     <div className="App">
-      <h1>test</h1>
+      <h1>{ accountUid }</h1>
     </div>
   );
 }
