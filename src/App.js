39fa8258cc88adbcd6449745d9d3f88feb5a31f8
afyqeyzar <norafyqe@gmail.com>
2023-03-29 18:34:03 +0100

    projected important data to webpage

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import getAccountsAPI from "./APIMethods"
 import moment from 'moment';
 import './App.css'
 
+function centsToDollars(cents) {
+  const dollars = (cents / 100).toFixed(2);
+  return dollars;
+}
+
 const App = () => {
 
   // const STATIC_ACCOUNT_UID = 'b47101a3-dce8-48ea-89c5-ccb3ad486caf'
   const STATIC_CATEGORY_UID = '17b963c6-2665-4599-868f-28e1aa3425a7'
 
   const [accountUid, setAccountUid] = useState([])
+  const [identifiers, setIdentifiers] = useState([])
+  const [balance, setBalance] = useState({
+    amount: {currency: 'placeholder', minorUnits: 'placeholder'},
+    clearedBalance: {currency: 'placeholder', minorUnits: 'placeholder'},
+    pendingTransactions: {currency: 'placeholder', minorUnits: 'placeholder'}
+  })
 
   // GETTING DATES
   const moment = require('moment-timezone');
@@ -28,11 +39,12 @@ const App = () => {
     });
     const accountsIdentifiersData = await response.json();
 
-    console.log(accountsIdentifiersData)
+    setIdentifiers(accountsIdentifiersData)
+    // console.log(accountsIdentifiersData)
   };
 
   const getAccountsBalanceAPI = async () => {
-    const accountUid = await getAccountsAPI();
+    const accountUid = await getAccountsAPI(setAccountUid);
     const response = await fetch(`/api/v2/accounts/${accountUid}/balance`, {
       method: "GET",
       headers: {
@@ -41,7 +53,8 @@ const App = () => {
     });
     const accountsBalanceData = await response.json();
 
-    console.log(accountsBalanceData)
+    setBalance(accountsBalanceData);
+    console.log(accountsBalanceData.clearedBalance)
   };
 
   const getAccountsFeedAPI = async () => {
@@ -60,6 +73,7 @@ const App = () => {
 
   useEffect(() => {
     getAccountsIdentifiersAPI();
+    getAccountsBalanceAPI();
     // getAccountsFeedAPI();
     // console.log(moment().format());
   }, []);
@@ -67,7 +81,23 @@ const App = () => {
   return (
     <div className="App">
       <h1>Account Uid: { accountUid }</h1>
-      <h2>test</h2>
+
+      <div>
+        <h1>Account Info</h1>
+        <h2>Account Number: { identifiers.accountIdentifier }</h2>
+        <h2>Sort Code: { identifiers.bankIdentifier }</h2>
+        <h2>IBAN: { identifiers.iban }</h2>
+        <h2>BIC: { identifiers.bic }</h2>
+      </div>
+
+      <div> 
+        <h1>Balances</h1>
+        <h2>Available Balance: { centsToDollars(balance.amount.minorUnits)} {balance.amount.currency}</h2>
+        <h2>Cleared Balance: { centsToDollars(balance.clearedBalance.minorUnits)} {balance.clearedBalance.currency}</h2>
+        <h2>Pending Balance: { centsToDollars(balance.pendingTransactions.minorUnits)} {balance.pendingTransactions.currency}</h2>
+      </div>
+
+
     </div>
   );
 }
