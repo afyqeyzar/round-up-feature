2e49481631ffab722296994a72365328b8df9132
afyqeyzar <norafyqe@gmail.com>
2023-04-01 15:38:10 +0100

    testing DateMethods.js

--- src/Methods/ButtonMethods.js
+++ src/Methods/ButtonMethods.js
@@ -5,6 +5,7 @@ import { getSum } from "./RoundUpMethod"
 import putSavingsGoal from "./TransferToSavings"
 
 const calculateButton = (setFeed, startDate, setSum, setBalance, setAccountSpecs, setSavingsGoal) => {
+  console.log(startDate)
   getSum(setFeed, apiDateFormat(startDate), apiDateFormat(addSevenDays(startDate)), setSum)
   loadBalances(setBalance, setAccountSpecs, setSavingsGoal)
   // makeSavingsGoal()
