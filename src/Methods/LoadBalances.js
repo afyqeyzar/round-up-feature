708db3b3a3f9aa4aecf9f8e076734b03206572ad
afyqeyzar <norafyqe@gmail.com>
2023-04-02 19:47:22 +0100

    updated dependencies

--- src/Methods/LoadBalances.js
+++
@@ -1,9 +0,0 @@
-import { getAccountsBalanceAPI, getSavingsGoal } from "./APIMethods";
-
-const loadBalances = (setBalance, setAccountSpecs, setSavingsGoal) => {
-  getAccountsBalanceAPI(setBalance);
-  getSavingsGoal(setAccountSpecs, setSavingsGoal);
-  // console.log(savingsGoal)
-}
-
-export default loadBalances
